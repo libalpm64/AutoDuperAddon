536c4b5ae781b411c8532ad3930bbc48391b4c1e
libalpm64 <159192189+libalpm64@users.noreply.github.com>
2025-12-30 17:44:33 -0500

    Inject custom rendering into DebugHud

--- src/main/java/com/example/addon/mixin/DebugHudMixin.java
+++ src/main/java/com/example/addon/mixin/DebugHudMixin.java
@@ -2,25 +2,23 @@ package com.example.addon.mixin;
 
 import com.example.addon.modules.CoordinateSpoofer;
 import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.DebugHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import java.util.List;
 import java.util.ListIterator;
 
 @Mixin(DebugHud.class)
 public class DebugHudMixin {
-    @Inject(method = "getLeftText", at = @At("RETURN"))
-    private void onGetLeftText(CallbackInfoReturnable<List<String>> info) {
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/class_340;method_51745(Lnet/minecraft/class_332;Ljava/util/List;Z)V", ordinal = 0, shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void onRender(DrawContext context, CallbackInfo ci, List<String> leftText) {
         CoordinateSpoofer module = Modules.get().get(CoordinateSpoofer.class);
         if (module == null || !module.isActive()) return;
-
-        List<String> list = info.getReturnValue();
-        ListIterator<String> it = list.listIterator();
-
+        ListIterator<String> it = leftText.listIterator();
         while (it.hasNext()) {
             String s = it.next();
             if (s.startsWith("XYZ: ")) {
